5219c596af6a6e8b33dee5bfdb3594f71db7c802
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-10 11:10:07 -0500

    added jsx files

--- Client/src/Components/Main.jsx
+++ Client/src/Components/Main.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import "bulma/css/bulma.min.css";
 
 export default function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="Main">
+      <div>
+        <section class="has-text-centered mt-5">
+          <h1 class="title is-center">I want to learn . . .</h1>
+          <p>search bar goes here</p>
+        </section>
+
+        <section class="has-text-centered mt-5">
+          <h1 class="title">Join the Community</h1>
+          <button class="button is-info">Become a Member</button>
+          <br />
+          <div class="mt-5">
+            <img src="#" alt="placeholder" />
+            <img src="#" alt="placeholder" />
+            <img src="#" alt="placeholder" />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
