9b5b4bbab292449a15b0ad354c6f8b29b33f5cba
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-11 12:51:05 -0500

    changed styling for post

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -4,52 +4,14 @@ import "../css/post.scss";
 // Temporarily hardcoded Posts
 export default function Post() {
   return (
-    // <div className="container mb-6 has-background-grey-lighter">
-    //   <div className="column profile-pic">
-    //     {/* TEMPORARY PLaceholder Image*/}
-    //     <div className="card is-horizontal">
-    //       <div className="card-image">
-    //         <figure className="image is-rounded">
-    //           <img
-    //             src="http://placehold.jp/3d4070/ffffff/150x150.png"
-    //             alt="placeholder image"
-    //           />
-    //         </figure>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   <div className="card-stacked">
-    //     <div className="card-content">
-    //       <div className="media-content">
-    //         <p className="subtitle is-5 is-italic has-text-weight-semibold is-pulled-left">
-    //           BUDDY
-    //         </p>
-
-    //         <p className="has-text-right">Gus April 22, 2022</p>
-    //       </div>
-    //       <div className="content is-pulled-left has-background-grey-lighter">
-    //         <p className="title is-4 mt-4">
-    //           I NEED A STUDY BUDDY. I JUST STARTED JAVASCRIPT.
-    //         </p>
-    //         <p className="comment-text">
-    //           I just started Codecademy. Prefer someone in the same timezone -
-    //           GMT. Email me at gus123@gmail.com.
-    //         </p>
-    //         <a href="#">24 comments</a>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
+   
     <div className="container">
       <div className="column">
         <div className="card is-horizontal">
           <div className="card-image">
             <figure className="image">
               <img
-                // className= "is-rounded"
-                src="http://placehold.jp/3d4070/ffffff/150x150.png"
+                src="http://placehold.jp/3d4070/ffffff/150x150.png" //src will change
                 alt="Placeholder image"
               />
             </figure>
@@ -59,15 +21,18 @@ export default function Post() {
               <div className="media-content">
                 <p className="subtitle is-6 has-text-left is-italic has-text-weight-semibold">
                   BUDDY
+                  {/* will change */}
                 </p>
                 <p className="title is-4 has-text-left">
                   I NEED A STUDY BUDDY. I JUST STARTED JAVASCRIPT.
+                  {/* will change */}
                 </p>
               </div>
 
               <div className="content has-text-left mt-2">
                 I just started Codecademy. Prefer someone in the same timezone -
                 GMT. Email me at gus123@gmail.com
+                {/* will change */}
                 <br />
                 <time datetime="2022-02-02">12:09 PM - 2 Feb 2022</time>
               </div>
@@ -75,6 +40,7 @@ export default function Post() {
             <footer className="card-footer">
               <div className="card-footer-item has-text-left">
                 <a href="#">24 comments</a>
+                {/* comments will change */}
               </div>
             </footer>
           </div>
