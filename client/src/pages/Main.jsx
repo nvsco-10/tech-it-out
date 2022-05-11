b3385cf44b4d5cd22df3b5257090d415f29bb015
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-11 09:19:04 -0500

    moved around some files for organization

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 
 export default function Main() {
