268c416eeecf05eb8e586506f91ed8aac52fcbe3
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-11 12:37:15 -0500

    fixing post styling

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,60 +1,82 @@
 import React from "react";
 // import "bulma/css/bulma.min.css";
-import { FaGithub } from "react-icons/fa";
-
+import "../css/post.scss";
 // Temporarily hardcoded Posts
 export default function Post() {
   return (
-    <div className="container mb-6 has-background-grey-lighter">
-      <div className="column profile-pic">
-        {/* TEMPORARY PLaceholder Image*/}
+    // <div className="container mb-6 has-background-grey-lighter">
+    //   <div className="column profile-pic">
+    //     {/* TEMPORARY PLaceholder Image*/}
+    //     <div className="card is-horizontal">
+    //       <div className="card-image">
+    //         <figure className="image is-rounded">
+    //           <img
+    //             src="http://placehold.jp/3d4070/ffffff/150x150.png"
+    //             alt="placeholder image"
+    //           />
+    //         </figure>
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div className="card-stacked">
+    //     <div className="card-content">
+    //       <div className="media-content">
+    //         <p className="subtitle is-5 is-italic has-text-weight-semibold is-pulled-left">
+    //           BUDDY
+    //         </p>
+
+    //         <p className="has-text-right">Gus April 22, 2022</p>
+    //       </div>
+    //       <div className="content is-pulled-left has-background-grey-lighter">
+    //         <p className="title is-4 mt-4">
+    //           I NEED A STUDY BUDDY. I JUST STARTED JAVASCRIPT.
+    //         </p>
+    //         <p className="comment-text">
+    //           I just started Codecademy. Prefer someone in the same timezone -
+    //           GMT. Email me at gus123@gmail.com.
+    //         </p>
+    //         <a href="#">24 comments</a>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="container">
+      <div className="column">
         <div className="card is-horizontal">
           <div className="card-image">
-            <figure className="image is-rounded">
+            <figure className="image">
               <img
+                // className= "is-rounded"
                 src="http://placehold.jp/3d4070/ffffff/150x150.png"
-                alt="placeholder image"
+                alt="Placeholder image"
               />
             </figure>
           </div>
-        </div>
-      </div>
-      <div className="card-stacked">
-        <div className="card-content">
-          <div className="media-content">
-            <div className="top">
-              <p className="subtitle is-5 is-italic has-text-weight-semibold is-pulled-left">
-                BUDDY
-              </p>
-              
-              <p className="has-text-right">
-                Gus April 22, 2022
-              </p>
-            </div>
-            <div className="title is-4 has-text-left mt-4">
-              I NEED A STUDY BUDDY. I JUST STARTED JAVASCRIPT.
-            </div>
-          </div>
+          <div className="card-stacked">
+            <div className="card-content">
+              <div className="media-content">
+                <p className="subtitle is-6 has-text-left is-italic has-text-weight-semibold">
+                  BUDDY
+                </p>
+                <p className="title is-4 has-text-left">
+                  I NEED A STUDY BUDDY. I JUST STARTED JAVASCRIPT.
+                </p>
+              </div>
 
-          <div className="column">
-            <span className=""> </span>
-          </div>
-        </div>
-
-        <div className="card-content">
-          <div className="content has-text-justified">
-            I just started Codecademy. Prefer someone in the same timezone -
-            GMT. Email me at gus123@gmail.com.
-          </div>
-          <div className="is-pulled-right">
-            <b>user.username</b>
-            <a href="https://twitter.com/{{user.twitter}}">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="https://github.com/{{user.github}}">
-              <FaGithub></FaGithub>
-            </a>
-            <a href="#">24 comments</a>
+              <div className="content has-text-left mt-2">
+                I just started Codecademy. Prefer someone in the same timezone -
+                GMT. Email me at gus123@gmail.com
+                <br />
+                <time datetime="2022-02-02">12:09 PM - 2 Feb 2022</time>
+              </div>
+            </div>
+            <footer className="card-footer">
+              <div className="card-footer-item has-text-left">
+                <a href="#">24 comments</a>
+              </div>
+            </footer>
           </div>
         </div>
       </div>
--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
 import "../css/style.css";
-import { FaGithub } from "react-icons/fa";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Post from "../components/Post";
@@ -34,7 +33,7 @@ export default function Main() {
           <Post></Post>
 
           {/* POST 2 */}
-          <div className="card mb-6">
+          {/* <div className="card mb-6">
             <header className="card-header card-header-title has-background-grey-lighter">
               <div className="column">
                 <p className=" has-text-primary-light">
@@ -68,46 +67,46 @@ export default function Main() {
                 <a href="#">24 comments</a>
               </div>
             </div>
-          </div>
+          </div> */}
 
           {/* POST 3 */}
-          <div className="card mb-6">
-            <header className="card-header card-header-title has-background-grey-lighter">
-              <div className="column">
-                <p className=" has-text-primary-light">
-                  <a
-                    href="/post/2" // would actually be /post/id
-                    className="has-text-black is-capitalized is-pulled-left"
-                  >
-                    I'm willing to teach a beginner for free
+            {/* <div className="card mb-6">
+              <header className="card-header card-header-title has-background-grey-lighter">
+                <div className="column">
+                  <p className=" has-text-primary-light">
+                    <a
+                      href="/post/2" // would actually be /post/id
+                      className="has-text-black is-capitalized is-pulled-left"
+                    >
+                      I'm willing to teach a beginner for free
+                    </a>
+                  </p>
+                </div>
+
+                <div className="column">
+                  <span className=""> </span>
+                </div>
+              </header>
+
+              <div className="card-content">
+                <div className="content has-text-justified">
+                  Must be motivated and have initiative. Don't care where you
+                  live.
+                </div>
+                <div className="is-pulled-right">
+                  <b>user.username</b>
+                  <a href="https://twitter.com/{{user.twitter}}">
+                    <i className="fab fa-twitter"></i>
                   </a>
-                </p>
+                  <a href="https://github.com/{{user.github}}">
+                    <FaGithub></FaGithub>
+                  </a>
+                  April 22, 2022
+                  <a href="#">24 comments</a>
+                </div>
               </div>
-
-              <div className="column">
-                <span className=""> </span>
-              </div>
-            </header>
-
-            <div className="card-content">
-              <div className="content has-text-justified">
-                Must be motivated and have initiative. Don't care where you
-                live.
-              </div>
-              <div className="is-pulled-right">
-                <b>user.username</b>
-                <a href="https://twitter.com/{{user.twitter}}">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="https://github.com/{{user.github}}">
-                  <FaGithub></FaGithub>
-                </a>
-                April 22, 2022
-                <a href="#">24 comments</a>
-              </div>
-            </div>
+            </div> */}
           </div>
-        </div>
       </div>
 
       <div>
