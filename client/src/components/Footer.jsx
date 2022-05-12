0a5b09f67886504e5a88cc12f1a6ecd72adc21e8
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-12 10:22:33 -0500

    footer styling done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import "../css/footer.scss"
+import "../css/footer.scss";
+import "@fontsource/roboto";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div>
       <footer className="footer">
-        <div className="content has-text-centered is-italic">
-          <p>Suggest a resource - suggestions@learnwebdev.com</p>
+        <div className="content has-text-centered">
+          <p>
+            Suggest a resource -{" "}
+            <a
+              href="suggestions@learnwebdev.com"
+              className="suggest-link has-text-white"
+            >
+              suggestions@learnwebdev.com
+            </a>
+          </p>
           <div className="navbar-menu has-text-centered">
-            <a className="navbar-item"><Link to="/contact">Contact</Link></a>
-            <a className="navbar-item"><Link to="/about">About Us</Link></a>
-            <a className="navbar-item">Privacy Policy</a>
+            <a className="navbar-item">
+              <Link className="has-text-white" to="/contact">
+                CONTACT
+              </Link>
+            </a>
+            <a className="navbar-item">
+              <Link className="has-text-white" to="/about">
+                ABOUT US
+              </Link>
+            </a>
+            <a className="navbar-item">PRIVACY POLICY</a>
           </div>
           <p>copyright 2022</p>
         </div>
