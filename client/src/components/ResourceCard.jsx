c03d9ed781e9a11731d708a374ef68df62951fad
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-13 18:54:56 -0500

    finished main styling on resource page also finished styling on single resource page

--- client/src/components/ResourceCard.jsx
+++ client/src/components/ResourceCard.jsx
@@ -1,41 +1,40 @@
-import React from 'react'
+import React from "react";
+import placeholder from "../images/laptop-colorful-stock-photo.jpg"
 import { Link } from "react-router-dom";
 
-const ResourceCard = ({id, name, description, type, level}) => {
+const ResourceCard = ({ id, name, description, type, level }) => {
   return (
-        <div class="column is-4-tablet is-3-desktop">
-            <div class="card">
-                <div class="card-image has-text-centered">
-                    <figure class="image is-2by1">
-                        <img
-                        src="http://placehold.jp/ff3860/ffffff/546x273.png"
-                        alt="Placeholder image"
-                        />
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-content">
-                            <Link to={`/resources/${id}`}>
-                                <p class="title is-5">{name.toUpperCase()}</p>
-                            </Link>
-                        <div>
-                            <a class="button is-danger mr-2">{type.toUpperCase()}</a>
-                            <a class="button is-success mr-2">{level.toUpperCase()}</a>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="content">
-                        <div class="media-content">
-                            <p class="is-success">
-                                {description}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="resource-card column is-4-tablet is-3-desktop">
+      <div class="card">
+        <div class="card-image has-text-centered">
+          <figure class="image is-2by1">
+            <img
+              src={placeholder}
+              alt="Placeholder image"
+            />
+          </figure>
         </div>
-  )
-}
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <Link to={`/resources/${id}`}>
+                <p class="title is-5 mb-2">{name.toUpperCase()}</p>
+              </Link>
+              <div>
+                <a class="button is-danger mr-2">{type.toUpperCase()}</a>
+                <a class="button is-success mr-2">{level.toUpperCase()}</a>
+              </div>
+            </div>
+          </div>
+          <div class="content">
+            <div class="media-content">
+              <p class="is-success">{description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ResourceCard
+export default ResourceCard;
--- client/src/pages/SingleResource.jsx
+++ client/src/pages/SingleResource.jsx
@@ -1,134 +1,156 @@
 // TEST PAGE
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import { getResourceById, createComment } from '../utils/API';
-import Auth from '../utils/auth';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getResourceById, createComment } from "../utils/API";
+import Auth from "../utils/auth";
 // import "../css/main.scss
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import image from "../images/laptop-colorful-stock-photo.jpg"
+import image from "../images/laptop-colorful-stock-photo.jpg";
 
 const SingleResource = () => {
-    const { id } = useParams();
-    const [ resourceData, setResourceData ] = useState({})
-    const [ commentData, setCommentData ] = useState({
-        comment: '',
-        username: ''
-    })
+  const { id } = useParams();
+  const [resourceData, setResourceData] = useState({});
+  const [commentData, setCommentData] = useState({
+    comment: "",
+    username: "",
+  });
 
-    const isLoggedIn = Auth.loggedIn();
-  
-    const getResource = async () => {
-      const data = await getResourceById(id);
-      const result = await data.json();
-      setResourceData(result)
-    }
-  
-    useEffect(() => {
-        getResource()
-    }, [])
-  
-    useEffect(() => {
-      const getUserData = async () => {
-        try {
-  
-          if (!isLoggedIn) {
-            return false;
-          }
-  
-          const response = await Auth.getProfile();
-  
-          if (!response) {
-            throw new Error('something went wrong!');
-          }
-  
-          // setUserData(response.data);
-          setCommentData({ ...commentData, 'username': response.data.username });
-  
-        } catch (err) {
-          console.error(err);
+  const isLoggedIn = Auth.loggedIn();
+
+  const getResource = async () => {
+    const data = await getResourceById(id);
+    const result = await data.json();
+    setResourceData(result);
+  };
+
+  useEffect(() => {
+    getResource();
+  }, []);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        if (!isLoggedIn) {
+          return false;
         }
-      };
-  
-      getUserData();
-    }, []);
-  
-    const handleInputChange = (event) => {
-      const { name, value } = event.target;
-      setCommentData({ ...commentData, [name]: value });
-  
-    }
-  
-    const handleSubmit = async e => {
-        e.preventDefault();
-  
-        console.log(commentData)
-  
-        const newComment = await createComment(commentData, id)
-  
-        if (!newComment) {
-          throw new Error('something went wrong!');
+
+        const response = await Auth.getProfile();
+
+        if (!response) {
+          throw new Error("something went wrong!");
         }
-  
-        console.log('comment added!')
-  
-        getResource()
-        setCommentData({ ...commentData, 'comment': '' });
-  
+
+        // setUserData(response.data);
+        setCommentData({ ...commentData, username: response.data.username });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUserData();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setCommentData({ ...commentData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(commentData);
+
+    const newComment = await createComment(commentData, id);
+
+    if (!newComment) {
+      throw new Error("something went wrong!");
     }
-    // TEST LAYOUT BELOW - CHANGE FUGLY STYLING 
-    return (
-      <div className='SingleResource'>
 
-        <Header></Header>
-  
-          <div className='resource-image'>
-              <img src={image} />
-              <h1
-              className='title'>{resourceData.name}</h1>
-              <p>{resourceData.type}</p>
-              <p>{resourceData.level}</p>
-              <p>{resourceData.description}</p>
-              <a href={resourceData.link} target='_blank' rel="noopener noreferrer">{resourceData.link}</a>
+    console.log("comment added!");
+
+    getResource();
+    setCommentData({ ...commentData, comment: "" });
+  };
+  // TEST LAYOUT BELOW - CHANGE FUGLY STYLING
+  return (
+    <div className="SingleResource">
+      <Header></Header>
+
+      <div className="resource-container">
+        <img src={image} />
+
+        <div className="singleresource-body">
+          <h1 className="title">{resourceData.name}</h1>
+          <div className="filter">
+            <p className="button is-danger mr-2">{resourceData.type}</p>
+            <p className="button is-success ml-2">{resourceData.level}</p>
           </div>
-  
-          <div>
-              <h1>Comments</h1>
-
-              {/* Only logged in users can comment */}
-              {isLoggedIn ? (
-                <>
-                  <input 
-                      name="comment"
-                      type="text"
-                      placeholder='add a comment' 
-                      value={commentData.comment}
-                      onChange={handleInputChange }/>
-                  <button onClick={handleSubmit}>Submit</button>
-                </>
-              ) : (
-                <p><a href='/login'>Login</a> to post a comment</p>
-              )}
-
-              <div>
-                  {resourceData.comments?.map(item => {
-                      return (
-                        // key property must be added to parent
-                        // insert comment component below
-                          <div key={item.commentId}>
-                              <p>{item.comment}</p>
-                              <p>{item.username}</p>
-                              <p>{item.createdAt}</p>
-                          </div>
-                      )
-                  })
-                  }
-              </div>
+          <div className="description-body">
+            <p className="resource-description">{resourceData.description}</p>
+            <a
+              href={resourceData.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Link: {resourceData.link}
+            </a>
           </div>
-
-          <Footer></Footer>
+        </div>
       </div>
-    )
-}
-  
-  export default SingleResource
+
+      <hr className="line" />
+
+      <div className="resource-comments">
+        <div className="hold-comments">
+          <h1 className="title">Leave a Comment</h1>
+
+          {/* Only logged in users can comment */}
+          {isLoggedIn ? (
+            <>
+              <div className="add-comment">
+                <input
+                  name="comment"
+                  type="text"
+                  placeholder="add a comment"
+                  value={commentData.comment}
+                  onChange={handleInputChange}
+                />
+                <div className="button-holder">
+                  <button className="button" onClick={handleSubmit}>
+                    POST
+                  </button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <p className="login-to">
+              <a href="/login">Login</a>&nbsp;to post a comment
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="comment-holder">
+        {resourceData.comments?.map((item) => {
+          return (
+            // key property must be added to parent
+            // insert comment component below
+            <div key={item.commentId}>
+              <div className="comment-info">
+                <p className="username">{item.username}</p>
+                <p className="time">{item.createdAt}</p>
+              </div>
+              <p className="comment">{item.comment}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default SingleResource;
