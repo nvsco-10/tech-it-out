b359cb22ed8e5b130d97040c66f70ac6ba1e45fe
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-14 14:59:56 -0500

    minor styling fixes error and bug fixes

--- client/src/components/ResourceCard.jsx
+++ client/src/components/ResourceCard.jsx
@@ -4,31 +4,31 @@ import { Link } from "react-router-dom";
 
 const ResourceCard = ({ id, name, description, type, level }) => {
   return (
-    <div class="resource-card column is-4-tablet is-3-desktop">
-      <div class="card">
-        <div class="card-image has-text-centered">
-          <figure class="image is-2by1">
+    <div className="resource-card column is-4-tablet is-3-desktop">
+      <div className="card">
+        <div className="card-image has-text-centered">
+          <figure className="image is-2by1">
             <img
               src={placeholder}
               alt="Placeholder image"
             />
           </figure>
         </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
               <Link to={`/resources/${id}`}>
-                <p class="title is-5 mb-2">{name.toUpperCase()}</p>
+                <p className="title is-5 mb-2">{name.toUpperCase()}</p>
               </Link>
               <div>
-                <a class="button is-danger mr-2">{type.toUpperCase()}</a>
-                <a class="button is-success mr-2">{level.toUpperCase()}</a>
+                <a className="button is-danger mr-2">{type.toUpperCase()}</a>
+                <a className="button is-success mr-2">{level.toUpperCase()}</a>
               </div>
             </div>
           </div>
-          <div class="content">
-            <div class="media-content">
-              <p class="is-success">{description}</p>
+          <div className="content">
+            <div className="media-content">
+              <p className="is-success">{description}</p>
             </div>
           </div>
         </div>
